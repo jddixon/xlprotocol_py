85ccca075334de9ed209bb6d1bdbec74c640af5e
Jim Dixon <jddixon@gmail.com>
2017-06-01 14:05:32 -0700

    drop dist/* from repos, plus minor edits

--- xlprotocol/__init__.py
+++ xlprotocol/__init__.py
@@ -2,8 +2,8 @@
 
 """ Protocol library for python XLattice packages. """
 
-__version__ = '0.0.3'
-__version_date__ = '2017-02-24'
+__version__ = '0.0.4'
+__version_date__ = '2017-06-01'
 
 __all__ = ['__version__', '__version_date__', 'XLProtocolError', ]
 
